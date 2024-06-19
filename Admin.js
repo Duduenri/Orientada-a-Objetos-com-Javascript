c8b3202ab1b2d79e7cdbe19e91f61ea7759235d4
Duduenri <eduess290799@gmail.com>
2024-06-19 13:40:13 -0300

    Adicionando métodos

--- Admin.js
+++ Admin.js
@@ -4,8 +4,14 @@ class Admin extends User {
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {
         super(nome, email, nascimento, role, ativo);
     }
+
+    criarCurso(nomeDoCurso, vagas) {
+        return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`
+    }
 }
 
 const novoAdmin = new Admin('Eduardo', 'd@.com', '2020-01-01')
-console.log(novoAdmin)
-console.log(novoAdmin.exibirInfos())
+console.log(novoAdmin.criarCurso('JS', 20))
+
+// console.log(novoAdmin)
+// console.log(novoAdmin.exibirInfos())
