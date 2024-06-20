d0551cf398cdafdf702f8f25a526cbc3acdd289c
Duduenri <eduess290799@gmail.com>
2024-06-20 11:53:39 -0300

    metodos privados

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import Admin from "./Admin.js";
 const novoUser = new User('Dudu', 'd@m', '2024-19-06')
 console.log(novoUser.exibirInfos())
 
+const novoAdmin = new Admin('Thai', 'thai@.com', '2024-20-06')
+console.log(novoAdmin.exibirInfos())
+console.log(novoAdmin.nomeAdmin())
+
 // novoUser.#nome = 'Alterando de qualquer jeito'
 // console.log(novoUser.nome)
 // o _ é mais utilizado que o # para privar atributos no Typescript
