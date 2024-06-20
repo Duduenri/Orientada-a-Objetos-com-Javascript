b8a90c9f51988731f444c5a2ee616c991921b781
Duduenri <eduess290799@gmail.com>
2024-06-20 16:59:45 -0300

    Getters

--- User.js
+++ User.js
@@ -12,15 +12,36 @@ export default class User {
         this.#ativo = ativo
     }
 
-    #montaObjUser() {
-        return ({
-            nome: this.#nome,
-            email: this.#email,
-            nascimento: this.#nascimento,
-            role: this.#role,
-            ativo: this.#ativo
-    })
-}
+    get nome() {
+        //pode ter lógica de condicional
+        return this.#nome
+    }
+
+    get email() {
+        return this.#email
+    }
+
+    get nascimento() {
+        return this.#nascimento
+    }
+
+    get role() {
+        return this.#role
+    }
+
+    get ativo() {
+        return this.#ativo
+    }
+
+//     #montaObjUser() {
+//         return ({
+//             nome: this.#nome,
+//             email: this.#email,
+//             nascimento: this.#nascimento,
+//             role: this.#role,
+//             ativo: this.#ativo
+//     })
+// }
     exibirInfos() {
         const objUser = this.#montaObjUser()
         return `${objUser.nome}, ${objUser.email}, ${objUser.nascimento}, ${objUser.role}, ${objUser.ativo}`
--- index.js
+++ index.js
@@ -2,13 +2,11 @@ import User from "./User.js";
 import Docente from "./Docente.js";
 import Admin from "./Admin.js";
 
-const novoUser = new User('Dudu', 'd@m', '2024-19-06')
-console.log(novoUser.exibirInfos())
+// const novoUser = new User('Dudu', 'd@m', '2024-19-06')
+// console.log(novoUser.exibirInfos())
 
 const novoAdmin = new Admin('Thai', 'thai@.com', '2024-20-06')
-console.log(novoAdmin.exibirInfos())
-console.log(novoAdmin.nomeAdmin())
-
+console.log(novoAdmin.nome)
 // novoUser.#nome = 'Alterando de qualquer jeito'
 // console.log(novoUser.nome)
 // o _ é mais utilizado que o # para privar atributos no Typescript
